8c567e96a8b4efbbe2f3eee279f78cd7cfb554c2
Juan Ernesto Martínez <ernestomtzg3@gmail.com>
2023-02-01 14:35:46 -0600

    fmt trait implemetation

--- src/lib.rs
+++ src/lib.rs
@@ -1,51 +1,96 @@
 mod helpers;
-use std::{fmt::Debug, collections::hash_map::RandomState};
+use std::{
+    fmt::Debug,
+    fmt::{write, Display},
+};
 
 // main struct of N Dimentional generic array.
-    //the shape is denoted by the `shape` array where the length is the Rank of the Ndarray
-    //the actual values are stored in a flattend state in a rank 1 array
+//the shape is denoted by the `shape` array where the length is the Rank of the Ndarray
+//the actual values are stored in a flattend state in a rank 1 array
 
 #[derive(Debug, Copy, Clone)]
-pub struct Ndarr<T: Copy + Clone + Default, const N: usize, const R: usize>{
+pub struct Ndarr<T: Copy + Clone + Default, const N: usize, const R: usize> {
     pub data: [T; N],
-    pub shape: [usize; R]
+    pub shape: [usize; R],
 }
 
-impl  <T: Copy + Clone + Debug + Default, const N: usize, const R: usize>Ndarr<T,N,R> {
-    //TODO: implement errors 
-    pub fn new(data: [T; N], shape: [usize; R]) -> Result<Self,String>{
+impl<T: Copy + Clone + Debug + Default, const N: usize, const R: usize> Ndarr<T, N, R> {
+    //TODO: implement errors
+    pub fn new(data: [T; N], shape: [usize; R]) -> Result<Self, String> {
         let n = helpers::muliply_list(&shape, 1);
-        if data.len() ==  n{
-            Ok(Ndarr { data: data, shape: shape })
-        }else {
-            Err(format!("The number of elements of an Ndarray of shape {:?} is {}, and {} were provided.", shape, n, N))
+        if data.len() == n {
+            Ok(Ndarr {
+                data: data,
+                shape: shape,
+            })
+        } else {
+            Err(format!(
+                "The number of elements of an Ndarray of shape {:?} is {}, and {} were provided.",
+                shape, n, N
+            ))
         }
     }
-    pub fn rank(self)-> usize{
+    pub fn rank(self) -> usize {
         R
     }
-    pub fn shape(self)->[usize; R]{
+    pub fn shape(self) -> [usize; R] {
         self.shape
     }
-    
 }
-trait Bimap<F>{
-    fn bimap(self, other: Self, f: F)-> Self;
+
+impl<T: Copy + Clone + Debug + Default + Display, const N: usize, const R: usize> Display
+    for Ndarr<T, N, R>
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let strs = self.data.map(|x| x.to_string());
+        let binding = strs.clone().map(|s| s.len());
+        let max_size = binding.iter().max().unwrap();
+        let mut fmt_str: [String; N] = strs.map(|s| helpers::format_vla(s, max_size));
+        let mut splits = self.shape.clone();
+        splits.reverse();
+
+        fn slip_fromat<'a>(strings: &'a mut [String], splits: &'a [usize]) -> () {
+            if splits.len() == 0 {
+                return;
+            }
+            let l = helpers::muliply_list(splits, 1);
+            let n_splits = strings.len() / l;
+            for i in 0..n_splits {
+                let new_s: &mut [String] = &mut strings[i * l..(i + 1) * l];
+                new_s[0].insert_str(0, "[");
+                new_s[l - 1].push_str("]");
+                slip_fromat(new_s, &splits[1..]);
+            }
+            return;
+        }
+        // TODO: add new lines in the correct places to display it more numpy like
+        slip_fromat(&mut fmt_str[0..], &mut splits[..]);
+
+        let out = fmt_str.clone().join(" ");
+        write!(f, "Ndarr({})", out)
+    }
+}
+trait Bimap<F> {
+    fn bimap(self, other: Self, f: F) -> Self;
 }
 
 // Here we need to think about if valueble maybe checking for the same shape and return an option instead
-impl <F, T: Copy + Debug + Clone + Default, const N: usize, const R: usize>Bimap<F> for Ndarr<T, N, R> 
-    where F: Fn(&T,&T)->T,
+impl<F, T: Copy + Debug + Clone + Default, const N: usize, const R: usize> Bimap<F>
+    for Ndarr<T, N, R>
+where
+    F: Fn(&T, &T) -> T,
     [T; N]: Default,
 {
-    fn bimap(self, other: Self, f: F)-> Self {
-       let mut out: [T; N] = Default::default();
-        for i in 0..N{
+    fn bimap(self, other: Self, f: F) -> Self {
+        let mut out: [T; N] = Default::default();
+        for i in 0..N {
             out[i] = f(&self.data[i], &other.data[i])
         }
-       Ndarr { data: out , shape: self.shape }
+        Ndarr {
+            data: out,
+            shape: self.shape,
+        }
     }
-
 }
 
 trait Transpose {
@@ -53,28 +98,29 @@ trait Transpose {
 }
 
 // Generic transpose for array of rank R
-    // The helper functions use in here can be derive with some maths, but maybe there is a beeter way to do it.
-impl <T: Default + Copy + Clone, const N: usize, const R: usize>Transpose for Ndarr<T, N, R>
-    where [T; N]: Default,
-    [usize; R]: Default
-    
+// The helper functions use in here can be derive with some maths, but maybe there is a beeter way to do it.
+impl<T: Default + Copy + Clone, const N: usize, const R: usize> Transpose for Ndarr<T, N, R>
+where
+    [T; N]: Default,
+    [usize; R]: Default,
 {
     fn t(self) -> Self {
         let shape = self.shape.clone();
         let mut out_dim: [usize; R] = self.shape.clone();
         out_dim.reverse();
         let mut out_arr: [T; N] = Default::default();
-        for i in 0..N{
+        for i in 0..N {
             let mut new_indexes = helpers::get_indexes(&i, &shape);
             new_indexes.reverse();
             let new_pos = helpers::get_flat_pos(&new_indexes, &out_dim).unwrap();
             out_arr[new_pos] = self.data[i].clone();
         }
-        Ndarr{data: out_arr, shape: out_dim}
+        Ndarr {
+            data: out_arr,
+            shape: out_dim,
+        }
     }
-    
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -82,24 +128,23 @@ mod tests {
 
     #[test]
     fn constoctor_test() {
-        let arr = Ndarr::new([0, 1, 2, 3], [2,2]).expect("Error initializing");
-        assert_eq!(arr.shape(), [2,2]);
+        let arr = Ndarr::new([0, 1, 2, 3], [2, 2]).expect("Error initializing");
+        assert_eq!(arr.shape(), [2, 2]);
         assert_eq!(arr.rank(), 2)
     }
 
     #[test]
     fn bimap_test() {
-        let arr1 = Ndarr::new([0, 1, 2, 3], [2,2]).expect("Error initializing");
-        let arr2 = Ndarr::new([4, 5, 6, 7], [2,2]).expect("Error initializing");
+        let arr1 = Ndarr::new([0, 1, 2, 3], [2, 2]).expect("Error initializing");
+        let arr2 = Ndarr::new([4, 5, 6, 7], [2, 2]).expect("Error initializing");
         assert_eq!(arr1.bimap(arr2, |x, y| x + y).data, [4, 6, 8, 10])
     }
 
     #[test]
     fn transpose() {
-        let arr = Ndarr::new([0, 1, 2, 3, 4, 5, 6, 7], [2,2,2]).expect("Error initializing");
+        let arr = Ndarr::new([0, 1, 2, 3, 4, 5, 6, 7], [2, 2, 2]).expect("Error initializing");
+        println!("{}", arr);
         // same as arr.T.flatten() in numpy
         assert_eq!(arr.t().data, [0, 4, 2, 6, 1, 5, 3, 7])
     }
-
-
 }
