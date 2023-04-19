45c4dc6da64543cca335871551961585c6ddf105
Juan Ernesto Martínez <ernestomtzg3@gmail.com>
2023-04-19 17:37:29 -0600

    fix toml

--- src/lib.rs
+++ src/lib.rs
@@ -297,6 +297,8 @@ where
 #[cfg(test)]
 mod tests {
 
+    use crate::utils::rapl_img;
+
     use super::*;
     use ops::*;
 
@@ -577,10 +579,5 @@ mod tests {
         assert_eq!(a_string.data, vec!["1".to_owned(), "2".to_owned(), "3".to_owned()]);
 
         println!("a_string: {:#?}", a_string); // ["1","2","3"]
-    }
-
-    #[test]
-    fn image_t(){
-        let img = open_rgb8(&"test_img.jpg");
     }
 }
