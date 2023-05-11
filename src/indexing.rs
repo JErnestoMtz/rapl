8285a0c58909d0a7c103a263a99fd1e6922d5909
Juan Ernesto Martínez <ernestomtzg3@gmail.com>
2023-05-11 13:13:48 -0600

    indexing module and assing at

--- src/indexing.rs
+++ src/indexing.rs
@@ -2,7 +2,8 @@ use super::*;
 
 
 impl<T: Clone + Debug + Default, R: Unsigned> Ndarr<T, R> {
-    pub fn assing_at<D: Into<Dim<R>>>(&mut self,index: D, value: T){
+    ///Modifies an element at an specific position by assign a new value.
+    pub fn assign_at<D: Into<Dim<R>>>(&mut self,index: D, value: T){
         let flat_pos = self.dim.get_flat_pos(&index.into()).unwrap();
         self.data[flat_pos] = value;
     }
@@ -14,9 +15,9 @@ mod indexing_tes{
     #[test]
     fn assing_at_t(){
         let mut arr = Ndarr::from([[1, 2],[3, 4]]);
-        arr.assing_at([0,1], 8);
+        arr.assign_at([0,1], 8);
         assert_eq!(&arr, &Ndarr::from([[1, 8],[3, 4]]));
-        arr.assing_at([1,1], 10);
+        arr.assign_at([1,1], 10);
         assert_eq!(&arr, &Ndarr::from([[1, 8],[3, 10]]));
     }
 }
